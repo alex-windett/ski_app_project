0e6cb11d74ac90534262529194cb9fe7122d6fb3
Alex Windett <alexwindett@hotmail.com>
2014-09-16 16:02:20 +0100

    masonary

---
+++ app/assets/javascripts/media.js
@@ -0,0 +1,21 @@
+  $(function(){
+    
+    $('#container').masonry({
+      itemSelector: '.box',
+      columnWidth: 100,
+      isAnimated: true,
+      isResizable: true
+    });
+    
+  });
+
+  //   $(function(){
+    
+  //   $('#container').imagesLoaded(function(){
+  //     $('#container').masonary({
+  //     itemSelector: '.box',
+  //     columnWidth: 100,
+  //     isAnimated: true
+  //   });
+    
+  // });
